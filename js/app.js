6a71b9e4844059e7fc8b8561bb70b844dae4d8fe
Icaruk <adri-rk@hotmail.com>
2019-10-31 09:24:29 +0100

    CSS casi listo

--- js/app.js
+++ js/app.js
@@ -1,34 +1,197 @@
-/*
-function muestraResultados () {
+
+function createNode (str) {
 	
-	const ele_resultados = uti.$("resultados");
+	let padre = document.createElement("div");
+	padre.innerHTML = str;
 	
-	ele_resultados.innerHTML = ""; // limpio
 	
-	
-	ele_resultados.appendChild;
-	
+	return padre.firstChild;
 	
 };
-*/
+
+
+
+async function busca(modo, busqueda) {
+	/*
+		busca("tracks", busqueda)
+		busca("users", busqueda)
+	*/
+	
+	
+	// Auth
+	SC.initialize({
+		client_id: 'aa06b0630e34d6055f9c6f8beb8e02eb',
+	});
+	
+	
+	// Limpio búsqueda
+	busqueda = busqueda.trim();
+	
+	
+	// Log
+	console.log ( `Petición (${modo}): ${busqueda}` ); 
+	
+	
+	// Petición
+	let promise = SC.get(`/${modo}`, {
+		q: busqueda,
+	});
+	
+	
+	// Return
+	return promise;
+	
+};
+
+
+
+function limpiaResultados() {
+	
+	// Limpio resultados anteriores
+	uti.$("resultados").innerHTML = "";
+	
+};
+
+
+
+function muestraTracks (info) {
+	
+	// Itero
+	maxIdx = info.length -1;
+	
+	
+	// Itero el objeto
+	for (let _i = 0; _i <= maxIdx; _i++) {
+		
+		let _x = info[_i];
+		
+		
+		// Saco algunos datos
+		let img = _x.artwork_url ? _x.artwork_url : "https://i1.sndcdn.com/avatars-000681921569-32qkcn-t500x500.jpg";
+		
+		
+		// Creo hijo
+		let nodo = createNode (`<div>
+			<h1>${_x.title}</h1>
+			<img class="img" src="${img}" />
+			${_x.user.username}
+			${_x.id}
+			
+		</div>`);
+		
+		console.log("nodo: ", nodo );
+		
+		
+		// Lo inyecto
+		uti.$("resultados").appendChild(nodo);
+		
+		
+	};
+	
+};
+
+
+
+function muestraUsers (info) {
+	
+	// Itero
+	maxIdx = info.length -1;
+	
+	
+	// Itero el objeto
+	for (let _i = 0; _i <= maxIdx; _i++) {
+		
+		let _x = info[_i];
+		
+		
+		// Saco algunos datos
+		let str_img = _x.avatar_url ? _x.avatar_url : "https://i1.sndcdn.com/avatars-000681921569-32qkcn-t500x500.jpg";
+		let str_description = _x.description ? _x.description : "";
+		let str_online = _x.online ? "ONLINE" : "OFFLINE";
+		
+		
+		// Creo hijo
+		let nodo = createNode (`<div>
+			<h1><a href="${_x.permalink_url}" target="_blank" rel="noopener noreferrer">${_x.username}</a></h1>
+			<img class="img" src="${str_img}" />
+			${str_online}
+			${str_description}
+			
+			Followers: ${_x.followers_count}
+			Following: ${_x.following_count}
+			
+		</div>`);
+		
+		
+		// Lo inyecto
+		uti.$("resultados").appendChild(nodo);
+		
+		
+	};
+	
+};
 
 
 
 
-SC.initialize({
-	client_id: 'aa06b0630e34d6055f9c6f8beb8e02eb',
+
+
+// EHs
+uti.$("input_tracks").addEventListener("keydown", (ev) => {
+	
+	if (ev.key == "Enter") {
+		
+		let busqueda = ev.target.value;
+		
+		busca("tracks", busqueda)
+		.then(function (res) {
+			
+			// Log
+			console.log( res );
+			
+			
+			// Limpio
+			limpiaResultados();
+			
+			
+			// Pinto
+			muestraTracks (res)
+				
+		});
+		
+		
+	};
+	
 });
 
 
-let autor = "asdf";
 
-SC.get('/tracks', {
-	q: autor,
-}).then(function (tracks) {
+uti.$("input_users").addEventListener("keydown", (ev) => {
 	
-	
-	
+	if (ev.key == "Enter") {
+		
+		let busqueda = ev.target.value;
+		
+		busca("users", busqueda)
+		.then(function (res) {
+			
+			// Log
+			console.log( res );
+			
+			
+			// Limpio
+			limpiaResultados();
+			
+			
+			// Pinto
+			muestraUsers (res)
+			
+		});
+		
+	};
 });
+
+
 
 
 /*
